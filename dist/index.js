dacfce2684e3b4c35e3fadd2e2a392c026dcc771
Steve Thompson <writetome51@gmail.com>
2019-03-31 14:53:59 -0600

    Updated the dependencies. Behavior of the function is the same.

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var errorIfIndex_howMany_andArray_areNotValid_1 = require("@writetome51/array-and-index-validation/errorIf/errorIfIndex_howMany_andArray_areNotValid");
-var errorIfArrayTooShortToMeetAdjacentItemsRequest_1 = require("@writetome51/array-and-index-validation/errorIf/errorIfArrayTooShortToMeetAdjacentItemsRequest");
+var error_if_index_how_many_or_array_are_not_valid_1 = require("error-if-index-how-many-or-array-are-not-valid");
+var error_if_array_too_short_to_meet_adjacent_items_request_1 = require("error-if-array-too-short-to-meet-adjacent-items-request");
 // Use this as a replacement for Array.splice() when removing items from array.
 // This replacement does strict type-checking for all 3 parameters.
 // startingIndex can be negative or positive.
 function getAndRemoveAdjacentAt(startingIndex, howManyToRemove, array) {
-    errorIfIndex_howMany_andArray_areNotValid_1.errorIfIndex_howMany_andArray_areNotValid(startingIndex, howManyToRemove, array);
-    errorIfArrayTooShortToMeetAdjacentItemsRequest_1.errorIfArrayTooShortToMeetAdjacentItemsRequest(startingIndex, howManyToRemove, array);
+    error_if_index_how_many_or_array_are_not_valid_1.errorIfIndex_howMany_orArray_areNotValid(startingIndex, howManyToRemove, array);
+    error_if_array_too_short_to_meet_adjacent_items_request_1.errorIfArrayTooShortToMeetAdjacentItemsRequest(startingIndex, howManyToRemove, array.length);
     if (howManyToRemove > 0)
         return array.splice(startingIndex, howManyToRemove);
     else
